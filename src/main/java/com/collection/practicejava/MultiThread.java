873f1417ec1d340df959fb6d6d04668964f0c550
akasshukla <akasshukla@paypal.com>
2017-05-29 19:15:49 +0530

    even odd two thread print

--- src/main/java/com/collection/practicejava/MultiThread.java
+++ src/main/java/com/collection/practicejava/MultiThread.java
@@ -5,22 +5,94 @@ package com.collection.practicejava;
  */
 public class MultiThread {
 
-    Runnable run = () ->
-            System.out.println("A");
-    Runnable run1 = () ->
-            System.out.println("B");
+    int i=1;
 
-    public static void main(String[] args) {
-        MultiThread m = new MultiThread();
-        m.doSomeThing();
-        System.out.println("main");
+    Runnable even = new Runnable() {
+        @Override
+        public void run() {
+            printEven();
+        }
+    };
+
+    private void printEven() {
+        synchronized (this){
+            while (i<=10){
+                if (i%2!=0){
+                    try {
+                        this.wait();
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                System.out.println("even :"+i);
+                i++;
+                this.notify();
+                }
+            }
+        }
+
     }
 
-    private void doSomeThing() {
-        Thread t = new Thread(run);
-        Thread t1 = new Thread(run1);
-        t.start();
-        t1.start();
+    Runnable odd = new Runnable() {
+        @Override
+        public void run() {
+            printOdd();
+        }
+    };
+
+    private void printOdd() {
+        synchronized (this){
+            while (i<=10){
+                if (i%2==0){
+                    try {
+                        this.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                System.out.println("odd "+i);
+                i++;
+                this.notify();
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
+
+    public static void main(String[] args) throws InterruptedException {
+        MultiThread multiThread=new MultiThread();
+        multiThread.doSomeThing();
+    }
+
+    private void doSomeThing() throws InterruptedException {
+        Thread threadEven = new Thread(even,"EVEN");
+        Thread threadOdd = new Thread(odd,"ODD");
+        threadEven.start();
+        threadOdd.start();
+    }
+
+    Runnable eventest = () -> {
+        int i=1;
+        while (i<=10){
+            if (i%2==0){
+                System.out.println("EVEN :"+i);
+            }
+                i++;
+        }
+    };
+    Runnable oddtest = () -> {
+        int i=1;
+        while (i<=10){
+            if (i%2!=0){
+                System.out.println("ODD "+i);
+            }
+                i++;
+        }
+    };
 
 }
