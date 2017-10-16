b1e6dc5e754c69ea305bc0bb5d1db5da447febd7
akashshukla1111 <akashshukla.69@gmail.com>
2017-10-17 00:34:53 +0530

    added hotstar interview question

--- src/test/java/com/hackerrank/paper/FindTheDecimalDigitTest.java
+++ src/test/java/com/hackerrank/paper/FindTheDecimalDigitTest.java
@@ -9,6 +9,18 @@ public class FindTheDecimalDigitTest {
 	@Test
 	public void findTheDigitAfterDecimal() throws Exception {
 
+		/*
+		Find the floting digit
+		first line input N
+		second line space separated integer values
+		return digit after decimal
+		ex :
+		input N=3
+		4 2   return "0"
+		14 3  return "inf"
+		13 4  return "25"
+		 */
+
 		FindTheDecimalDigit findTheDecimalDigit=new FindTheDecimalDigit();
 		Assert.assertEquals("inf" ,findTheDecimalDigit.findTheDigitAfterDecimal(36,7));
 		Assert.assertEquals("875" ,findTheDecimalDigit.findTheDigitAfterDecimal(95,8));
